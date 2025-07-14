bfa36a71ccb7487ef584b45fd213c07b89800020
Anna Ganisheva <anna.v.ganisheva@gmail.com>
2025-07-14 18:01:36 +0200

    added prefect

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,4 +2,5 @@
 variable "image_uri" {
   description = "Docker image URI (ECR або DockerHub)"
   type        = string
+  default     = "546727414131.dkr.ecr.eu-central-1.amazonaws.com/churn-app:latest"
 }
